9e96d23c55a9c0187a44f50152771af17bae7490
Filip Popovski <popovski_f@hotmail.com>
2023-01-22 20:03:28 +0100

    connecting backend and frontend

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,4 +16,23 @@ router.route("/").get((req, res) => {
   res.status(200).json({ message: "Hellow from dalle" });
 });
 
+router.route("/").post(async (res, req) => {
+  try {
+    const { prompt } = req.body;
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      response_format: "b64_json",
+      size: "1024x1024",
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err?.response.data.err.message);
+  }
+});
+
 export default router;
